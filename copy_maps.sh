3e3dba67dad68fd0acf49cece5e02a92c6d77883
Lambert Teisserenc <lambert.teisserenc@braineet.com>
2020-11-23 14:07:57 +0100

    remove useless scripte

--- copy_maps.sh
+++
@@ -1,4 +0,0 @@
-cp /home/steam/htdocs/cstrike/maps/* /home/steam/css/cstrike/maps/
-cp -r /home/steam/htdocs/cstrike/materials/ /home/steam/css/cstrike/
-
-./srcds_run -game cstrike +exec server.cfg +hostname "$CSS_HOSTNAME" +sv_password "$CSS_PASSWORD" +rcon_password "$RCON_PASSWORD" +map cs_belair73
